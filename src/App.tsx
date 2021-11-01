b8faeb6afe7a52806aba758928bac5aa22928052
Dominic Tölle <dominic.tolle@gmail.com>
2021-11-01 15:03:51 +0100

    Add React-Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.scss";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid className="page-container">
+      <Row>
+        <Col>
+          <Header></Header>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+        <Main></Main>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+        <Footer></Footer>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/Main.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Main() {
+  return <main>Middle</main>;
+}
+
+export default Main;
